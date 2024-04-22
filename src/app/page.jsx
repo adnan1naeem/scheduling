fad2c1148f523a4d444af88e4a11f896a3116941
Mac <mac@Usmans-Mac.local>
2024-04-22 16:04:11 +0500

    routing issue

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,12 @@
 // project import
 import GuestGuard from 'utils/route-guard/GuestGuard';
-import Login from 'views/auth/login';
+// import Login from 'views/auth/';
+import SamplePage from 'views/sample-page';
 
 export default function HomePage() {
   return (
     <GuestGuard>
-      <Login />
+      <SamplePage />
     </GuestGuard>
   );
 }
